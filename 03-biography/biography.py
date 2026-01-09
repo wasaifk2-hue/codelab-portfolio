e6ec21c02c8bdf8bdc5e9c21aec2eff6cbd3c600
wasaifk2-hue <wasaifk2@gmail.com>
2026-01-09 12:29:23 +0300

    Add biography program with personal information

---
+++ 03-biography/biography.py
@@ -0,0 +1,7 @@
+# This program shows a basic information
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+print("my name is wasaif Diab")
+print("I am 26 years old")
+print("my hobby is reading books")
