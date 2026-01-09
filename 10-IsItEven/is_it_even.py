46978ae2799c6fd8aec24c2084555f2760ccec59
wasaifk2-hue <wasaifk2@gmail.com>
2026-01-09 12:53:05 +0300

    Add program to check if a number is even or odd

---
+++ 10-IsItEven/is_it_even.py
@@ -0,0 +1,9 @@
+#this program checks if a number is even or odd
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
