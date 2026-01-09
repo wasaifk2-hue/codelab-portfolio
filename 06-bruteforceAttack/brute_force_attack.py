c01ab2155f0d01f3c2cdc0b6280e4fd161dcd8ee
wasaifk2-hue <wasaifk2@gmail.com>
2026-01-09 12:31:25 +0300

    Implement password brute force attack script
    Add a simple brute force password cracking program.

---
+++ 06-bruteforceAttack/brute_force_attack.py
@@ -0,0 +1,9 @@
+#this program keeps asking for password
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+password = "1234"
+guess = ""
+while guess != password:
+    guess = input("Enter password: ")
+    print("password cracked!")
