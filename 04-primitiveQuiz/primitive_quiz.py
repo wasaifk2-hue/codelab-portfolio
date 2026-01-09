e0f8931ae067a20b937a14d08be4303185fad68b
wasaifk2-hue <wasaifk2@gmail.com>
2026-01-09 12:30:03 +0300

    Add simple quiz program for capital of Sudan

---
+++ 04-primitiveQuiz/primitive_quiz.py
@@ -0,0 +1,9 @@
+# This program asks a simple question
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+answer = input("What is the capital of sudan? ")
+if answer.lower() == "khartoum":
+    print("Correct")
+else:
+    print("wrong ")
