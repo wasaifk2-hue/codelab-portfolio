21abf0932fb7a6ce2f2b6795e77a161de65c3425
wasaifk2-hue <wasaifk2@gmail.com>
2026-01-09 12:25:24 +0300

    Update simple_sums.py

---
+++ 02-simpilesums/simple_sums.py
@@ -0,0 +1,9 @@
+# This program adds two numbers
+# Author: wasaif Diab
+# Date: 29/12/2025
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+total = num1 + num2
+print("The sum is:", total)
